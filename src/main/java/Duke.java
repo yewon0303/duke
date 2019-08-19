556f3a896e91400f11e76ca3794d93d2781c7953
yewon0303 <yewon0303@gmail.com>
2019-08-19 19:18:51 +0800

    Level-1 Greet, Echo, Exit completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void greeting() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
+    private static void commandHandler() {
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            String command = scn.next();
+            if (command.equals("bye")) {
+                //exit when the user types bye
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                System.out.println();
+                break;
+            } else {
+                //echo the command entered by the user
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + command);
+                System.out.println("    ____________________________________________________________");
+                System.out.println();
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +39,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greeting();
+        commandHandler();
     }
 }
