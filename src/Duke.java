33ccf57b2c6f86e8f3c7018166da96e870b60302
yewon0303 <yewon0303@gmail.com>
2019-08-19 19:39:48 +0800

    Level-2 Add, List completed

--- src/Duke.java
+++ src/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class Duke {
     private static void greeting() {
@@ -10,26 +11,40 @@ public class Duke {
 
     private static void commandHandler() {
         Scanner scn = new Scanner(System.in);
-
+        LinkedList<String> commands = new LinkedList<>();
         while (true) {
-            String command = scn.next();
+            String command = scn.nextLine();
             if (command.equals("bye")) {
-                //exit when the user types bye
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
-                System.out.println();
+                bye();
                 break;
+            } else if (command.equals("list")) {
+                listAll(commands);
             } else {
+                //add command to the list of commands
+                commands.add(command);
                 //echo the command entered by the user
                 System.out.println("    ____________________________________________________________");
-                System.out.println("    " + command);
+                System.out.println("    added: " + command);
                 System.out.println("    ____________________________________________________________");
                 System.out.println();
             }
         }
+    }
 
+    private static void bye() {
+        //exit when the user types bye
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
 
+    private static void listAll(LinkedList<String> commands) {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < commands.size(); i++) {
+            System.out.println("    " + (i + 1) + ". " + commands.get(i));
+        }
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
