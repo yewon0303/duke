01fbcba4934313480ac2ec37de40deabc80f023e
yewon0303 <45944391+yewon0303@users.noreply.github.com>
2019-09-12 13:58:50 +0800

    Check CodeQuality and avoid nested functions (#7)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,34 +4,17 @@ import duke.TaskList;
 import duke.command.Command;
 import duke.ui.TextUi;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
-import javafx.scene.image.Image;
-
-
 public class Duke {
-    public TextUi ui;
-    public Storage storage;
-    public TaskList taskList;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/olivia.jpg"));
-    private Image Duke = new Image(this.getClass().getResourceAsStream("/images/dukeOrsino.png"));
-
+    private TextUi ui;
+    private Storage storage;
+    private TaskList taskList;
+    
     public Duke() {
         this.ui = new TextUi();
         this.storage = new Storage();
         this.taskList = storage.load();
     }
-
+  
     String getResponse(String input) {
         String nextCommand = input;
         Parser parser = new Parser();
--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -44,6 +44,126 @@ public class Parser {
     private Command oneWordCommandParser(String command) throws DukeException {
         Command returnCommand = new Command();
         switch (command) {
+            case "undo":
+                returnCommand = new UndoCommand();
+                break;
+            case "bye":
+                returnCommand = new ByeCommand();
+                break;
+            case "list":
+                returnCommand = new ListCommand();
+                break;
+            default:
+                throw new DukeException("I'm sorry, but I don't know what that means.");
+        }
+        return returnCommand;
+    }
+
+    /**
+     * Parses commands consisting of additional details, such as
+     * "find findIndex", "view viewIndex", "todo Task", "deadline Task /by deadline", "event Task /at date",
+     * "done doneIndex", "delete deleteIndex".
+     *
+     * @param fullCommand
+     * @param split
+     * @return command to be returned to the main parse function
+     * @throws DukeException
+     */
+    private Command multWordsCommandParser(String fullCommand, String[] split) throws DukeException {
+        String keyword = split[0];
+        Command returnCommand = new Command();
+        switch(keyword) {
+            case "find": //find all tasks containing keyword
+                String searchWord = split[1];
+                returnCommand = new FindCommand(searchWord);
+                break;
+            case "view":
+                int searchTask = Integer.parseInt(split[1]);
+                returnCommand = new ViewCommand(searchTask);
+                break;
+            case "todo": //create Todo Task
+                returnCommand = addCommandParser("todo", fullCommand, split);
+                break;
+            case "deadline": //create Deadline Task
+                returnCommand = addCommandParser("deadline", fullCommand, split);
+                break;
+            case "event": //create Event Task
+                returnCommand = addCommandParser("event", fullCommand, split);
+                break;
+            case "done": //mark Task done
+                if (split.length != 2) {
+                    throw new DukeException("Please enter in the format done [task index]");
+                } else {
+                    TaskList.addCommand(CommandEnum.Commands.DONE);
+                    int doneTask = Integer.parseInt(split[1]) - 1;
+                    returnCommand = new DoneCommand(doneTask);
+                    break;
+                }
+            case "delete": //delete Task
+                if (split.length != 2) {
+                    throw new DukeException("Please enter in the format delete [task index]");
+                } else {
+                    TaskList.addCommand(CommandEnum.Commands.DELETE);
+                    int wantDelete = Integer.parseInt(split[1]) - 1;
+                    returnCommand = new DeleteCommand(wantDelete);
+                    break;
+                }
+            default: throw new DukeException("I'm sorry, I don't understand you.");
+        }
+        return returnCommand;
+    }
+
+    /**
+     * Parses commands input by user that results in addition to tehe current TaskList.
+     *
+     * @param type
+     * @param fullCommand
+     * @param split
+     * @return command to be returned to the main parse function
+     * @throws DukeException
+     */
+    private Command addCommandParser(String type, String fullCommand, String[] split) throws DukeException {
+        Command returnCommand = new Command();
+        switch (type) {
+            case "todo": //Todo Task
+                TaskList.addCommand(CommandEnum.Commands.ADD);
+                Todo newTodo = new Todo(fullCommand.substring(5));
+                returnCommand = new AddCommand(newTodo);
+                break;
+            case "deadline": //Deadline Task
+                String[] parsedTask = fullCommand.substring(9).split("/by");
+                if (parsedTask.length != 2) {
+                    throw new DukeException("Please enter deadline in the format /by [deadline]");
+                } else {
+                    TaskList.addCommand(CommandEnum.Commands.ADD);
+                    Deadline newDeadline = new Deadline(parsedTask);
+                    returnCommand = new AddCommand(newDeadline);
+                    break;
+                }
+            case "event": //Event Task
+                String[] parsedTask1 = fullCommand.substring(6).split("/at");
+                if (parsedTask1.length != 2) {
+                    throw new DukeException("Please enter date or time in the format /at [date or time]");
+                } else {
+                    TaskList.addCommand(CommandEnum.Commands.ADD);
+                    Event newEvent = new Event(parsedTask1);
+                    returnCommand = new AddCommand(newEvent);
+                    break;
+                }
+            default: throw new DukeException("I'm sorry, I don't understand you.");
+        }
+    }
+
+    /**
+     * Parses one word command input by user such as Undo, Bye, List.
+     *
+     * @param command
+     * @return command to be returned to main parse function
+     * @throws DukeException
+     */
+    private Command oneWordCommandParser(String command) throws DukeException {
+        Command returnCommand = new Command();
+        switch (command) {
         case "undo":
             returnCommand = new UndoCommand();
             break;
@@ -153,5 +273,5 @@ public class Parser {
         }
         return returnCommand;
     }
-
+  
 }
